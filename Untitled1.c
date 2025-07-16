3b689bc4fd0c00718b7cf40dad47404375f0df3c
mrakib26 <mrakib2620@gmail.com>
2025-07-17 03:04:54 +0600

    added some codes

---
+++ Untitled1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Recursive function to find factorial
+long long factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+
+    // Input from user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d = %lld\n", num, factorial(num));
+    }
+
+    return 0;
+}
+
--- Untitled4.c
+++ Untitled4.c
@@ -5,32 +5,32 @@
 int main()
 {
     int num, count = 0, r, temp, sum = 0;
-    printf("Enter number: "); 
-    scanf("%d", &num); 
-    
-    int n = num; 
-    
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    int n = num;
+
     // Counting Digits.
     while(n!=0) {
         n = n/10;
         count++;
     }
-    
+
     // Calculating Sum of digits raised to the power of count.
-    temp = num; 
+    temp = num;
     while (temp!=0) {
-        r = temp%10; 
-        sum = sum + pow(r,count); 
-        temp = temp/10; 
+        r = temp%10;
+        sum = sum + pow(r,count);
+        temp = temp/10;
     }
 
     // Printing the output.
     if (sum==num) {
-        printf("%d is an Armstrong Number.\n", num); 
+        printf("%d is an Armstrong Number.\n", num);
     }
     else {
-        printf("%d is not an Armstrong Number.\n", num); 
+        printf("%d is not an Armstrong Number.\n", num);
     }
 
-    return 0; 
+    return 0;
 }
